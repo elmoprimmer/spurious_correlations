4f3e5c010c0da077fe4a1960d30497c92230ade2
Elmo Primmer <elmo.primmer@gmail.com>
2024-09-25 17:39:39 +0200

    Prettied it up a bit v3

--- norms.py
+++ norms.py
@@ -2,6 +2,11 @@ from surgeon_pytorch import Extract
 import torch
 import cv2
 import matplotlib.pyplot as plt
+import os
+import torchvision.transforms as transforms
+from PIL import Image
+
+
 
 
 class Norms:
@@ -53,8 +58,11 @@ class Norms:
     def plot_l2_norm_distribution_single(self, image, title="L2 Norm Distribution", bins=50):
         plt.figure(figsize=(5, 4))
 
-        norms = self.generate(image)[:,0,:]
+        norms = self.generate(image)
+
         print(norms.shape)
+        norms = norms[:,:,0].flatten()
+        print(norms)
         # Plot the L2 norm distribution
         plt.hist(norms, bins=bins, log=True, density=True, alpha=1)
         plt.title(title)
@@ -63,3 +71,39 @@ class Norms:
 
         plt.tight_layout()
         plt.show()
+
+
+
+    def tensorize(self, img_path):
+        preprocess = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        image = Image.open(img_path).convert('RGB')
+        return preprocess(image)
+
+    def plot_l2_norm_distribution_folder(self, folder_path, title="L2 Norm Distribution", bins=50):
+        all_norms = []
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".jpg") or filename.endswith(".png"):
+                image_path = os.path.join(folder_path, filename)
+
+                image = self.tensorize(image_path)
+                norms = self.generate(image)[:, :, 0].flatten()
+
+                all_norms.append(norms)
+
+        all_norms = torch.cat(all_norms, dim=0)
+
+        plt.figure(figsize=(5, 4))
+        plt.hist(all_norms.cpu().numpy(), bins=bins, log=True, density=True, alpha=1)
+        plt.title(title)
+        plt.xlabel(r'$L_2$ norm')
+        plt.ylabel(r'#')
+
+        plt.tight_layout()
+        plt.show()
+
